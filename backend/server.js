f4dd44e6e4749eb8b2006c36acce3b17d5372482
Azaan Suhail <azaan.suhail@gmail.com>
2025-10-15 02:10:32 +0530

    Testing

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,14 @@ import bodyParser from 'body-parser';
 import authRoutes from './routes/auth.routes.js';
 import productRoutes from './routes/product.route.js';
 import contactRoutes from './routes/contact.route.js';
+import path  from "path";
 
 dotenv.config();
 
 const PORT=process.env.PORT || 5000
 const app=express();  
+
+const __dirname=path.resolve()
 
 // Increase body size limit to 10mb (or more if needed)
 app.use(express.json({ limit: "10mb" })); //& This helps in parsing json data from the client
@@ -27,13 +30,16 @@ app.use('/api/auth',authRoutes);
 app.use('/api/products',productRoutes);
 app.use('/api/send-mail',contactRoutes);
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
+
 app.listen(PORT,()=>{
     console.log(`Server is running ✅ on http://localhost:${PORT}`);
     connectDB();
 })
 
-
-
-
-
-// LEARN : add the attributes for SEO optimization also, add the icon in the footer near the himalaya furniture house
