e140a3b093abf524714c428c9aa11b5249ba6fa7
Azaan Suhail <azaan.suhail@gmail.com>
2025-10-16 01:05:51 +0530

    testing 1.14

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
-router.get('/me', protectedRoute, me);
 
 
 export default router;
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -10,12 +10,7 @@ router.get('/category/:slug', getProductsByCategory);
 router.post('/createProduct', protectedRoute, adminRoute, createProduct);
 router.delete('/deleteProduct/:id', protectedRoute, adminRoute, deleteProduct);
 router.put('/:id', protectedRoute, adminRoute, updateProduct);
-router.put('/:id', protectedRoute, adminRoute, updateProduct);
 
-// // Wishlist routes
-// router.post('/wishlist/add', protectedRoute, addToWishlist);
-// router.post('/wishlist/remove', protectedRoute, removeFromWishlist);
-// router.get('/wishlist', protectedRoute, getWishlist);
 
 
 export default router;
